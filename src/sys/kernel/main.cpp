6dc38cf17643262a17c15d254769a8fde53b4a13
sksat <sksat.tec@gmail.com>
2017-03-27 23:14:44 +0900

    print message by TEXT-MODE VRAM

--- src/sys/kernel/main.cpp
+++ src/sys/kernel/main.cpp
@@ -1,17 +1,27 @@
-class a{
-private:
-	int b;
-	int c;
-public:
-	void set(int b, int c){this->b=b; this->c=c;};
-	void add(){b+=c;}
-};
+#include "../boot/multiboot2.h"
+
+#define VRAM_TEXTMODE	0x000B8000
+#define MAX_X		25
+#define COL_BLACK	0x00
+#define COL_WHITE	0x0F
+
+
+void puttext_vram(char *str, int len, int x, int y){
+	unsigned short *vram_t, col;
+	vram_t = (unsigned short*)VRAM_TEXTMODE;
+	col = (COL_BLACK << 4) | (COL_WHITE & 0x0f);
+	vram_t += x + y*MAX_X;
+	for(int i=0;i<len;i++){
+		vram_t++;
+		*vram_t = (col << 8) | str[i];
+	}
+}
 
 int main(void){
-	int hoge;
-	a a1;
-	a1.set(1,2);
-	a1.add();
+	char str[] = "welcome to HELIOS";
+	int len = sizeof(str);
+	puttext_vram(str, len, 0, 0);
+	for(;;);
 }
 
 
