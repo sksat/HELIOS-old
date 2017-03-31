2125f50d42c9e5328c74824ca6e4ce380c06bb97
sksat <sksat.tec@gmail.com>
2017-03-31 18:10:31 +0900

    add asmfunc and change graphic mode

---
+++ src/sys/kernel/asmfunc.h
@@ -0,0 +1,12 @@
+extern "C" {
+void io_hlt(void);
+void io_cli(void);
+void io_sti(void);
+void io_stihlt(void);
+int io_in8(int port);
+int io_in16(int port);
+int io_in32(int port);
+void io_out8(int port, int data);
+void io_out16(int port, int data);
+void io_out32(int port, int data);
+};
--- src/sys/kernel/main.cpp
+++ src/sys/kernel/main.cpp
@@ -1,4 +1,5 @@
 #include "../boot/multiboot2.h"
+#include "asmfunc.h"
 
 #define VRAM_TEXTMODE	0x000B8000
 #define MAX_X		80
@@ -17,6 +18,32 @@ void puttext_vram(char *str, int len, int x, int y){
 	}
 }
 
+void set_qemuvga0reg(int reg, int dat)
+{
+    io_out16(0x01ce, reg);
+    io_out16(0x01cf, dat);
+    return;
+}
+
+char* init_qemuvga0(int x, int y, int c, int flag)
+{
+    set_qemuvga0reg(0x0004, 0x0000);
+    set_qemuvga0reg(0x0001, x);
+    set_qemuvga0reg(0x0002, y);
+    set_qemuvga0reg(0x0003, c); /* 4, 8, 15, 16, 24, 32 */
+    set_qemuvga0reg(0x0005, 0x0000);
+    set_qemuvga0reg(0x0004, flag); /* リニアアクセスモードでVRAMの初期化をするなら0x41 */
+        /* bit7 : VRAM初期化抑制, bit6 : リニアアクセスモード, bit0 : 設定有効 */
+//    binfo->scrnx = x;
+//    binfo->scrny = y;
+//    binfo->vmode = c;
+    if ((flag & 0x40) == 0) {
+        return (char *) 0x000a0000;
+    } else {
+        return (char *) 0xe0000000;
+    }
+}
+
 int main(void){
 	char str[] = "welcome to HELIOS";
 	char str2[]= "this is a simple baremetal program by sksat.";
@@ -24,6 +51,9 @@ int main(void){
 	int len2= sizeof(str2);
 	puttext_vram(str, len, 0, 0);
 	puttext_vram(str2, len2, 0, 2);
+
+	init_qemuvga0(800, 600, 8, 0xc1);
+
 	for(;;);
 }
 
